c8c0e8b811093d67b59dc7ee268a285f6ee859cc
tancredi covioli <mr.coviols96@gmail.com>
2020-11-24 17:16:13 +0100

    optimizations to the docker script
    now it builds the image and the container only if necessary, avoiding unnecessary errors

--- dock-run.sh
+++ dock-run.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+if ! docker images --format "{{.Repository}}" | grep -q rhse; then
+  echo "building the image...";
+  docker build --tag=rhse ./rhse-docker
+fi
 
-docker build --tag=rhse ./rhse-docker && \
-docker run -d \
-  -it \
-  --name rhsec \
-  --mount type=bind,source=$(pwd)/Ride-Hailing-Service-Emulator,target=/rhse \
-  rhse:latest
+if ! docker container ls -a --format "{{.Names}}" | grep -q rhsec; then
+  echo "creating the container...";
+  docker run -d \
+    -it \
+    --name rhsec \
+    --mount type=bind,source=$(pwd)/Ride-Hailing-Service-Emulator,target=/rhse \
+    rhse:latest;
+else  
+  if ! docker ps --format "{{.Names}}" | grep -q rhsec; then
+    echo "starting the container...";
+    docker start rhsec;
+  fi
+fi
+docker exec -it rhsec bash;
